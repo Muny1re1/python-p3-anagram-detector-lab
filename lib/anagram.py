9c4c2d090498bd134df1794c516636d56b273fae
Muny1re1 <munyiriandy@gmail.com>
2024-05-26 19:36:09 +0300

    labDone

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+            self.word = word.lower()
+            self.sorted_word = sorted(self.word)
+
+    def match(self, list):
+        matched = []
+
+        for word in list:
+            if self.sorted_word == sorted(word.lower()) and self.word != word:
+                matched.append(word)
+                
+        return matched
+
+    
